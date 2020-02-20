d0a59ea3fdec7e0abca7a7d9c82aedd8a2fe4bae
kmanojreddy7797 <43980876+kmanojreddy7797@users.noreply.github.com>
2020-02-20 10:07:28 +0530

    Update SelectionSort.java
    Selection Sort

--- src/com/naren/sortings/SelectionSort.java
+++ src/com/naren/sortings/SelectionSort.java
@@ -1,28 +1,39 @@
-package com.naren.sortings;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class SelectionSort {
-	
+public class Main
+{
 	public static void main(String[] args) {
-		ArrayList<Integer> l = new ArrayList<>(Arrays.asList(1,5,4,3,2,1,10));
-		
-		for(int i=0;i<=l.size()-2;i++) {
-			int min=i;
-			for(int j=i+1;j<=l.size()-1;j++) {
-				if(l.get(min)>l.get(j)) {
-					min=j;
-				}
-			}
-			if(min != i) {
-				Integer temp=l.get(min);
-				l.set(min, l.get(i));
-				l.set(i, temp);
-			}
-		}
-		
-		l.stream().forEach(System.out::print);
-	}
+		System.out.println("Selection Sort");
+		/*
+		Time Complexity: O(n2) as there are two nested loops.
 
+		Auxiliary Space: O(1)
+        	The good thing about selection sort is it never makes more than O(n) 
+        	swaps and can be useful when memory write is a costly operation.
+		*/
+		int[] unsortedArray = {2,3,1,5,4,7,9,6,0};
+		System.out.println("Unsorted Array:");
+		printArray(unsortedArray);
+		int[] sortedArray;
+		for(int i=0;i<unsortedArray.length;i++){
+		    int minimumElementsIndex = i;
+		    for(int j = i + 1; j < unsortedArray.length; j++){
+		        if(unsortedArray[minimumElementsIndex]>unsortedArray[j]){
+		            minimumElementsIndex = j;
+		        }
+		    }
+		    if(unsortedArray[i] != unsortedArray[minimumElementsIndex]){
+    		    int temp = unsortedArray[i];
+    		    unsortedArray[i] = unsortedArray[minimumElementsIndex];
+    		    unsortedArray[minimumElementsIndex] = temp;
+		    }
+		}
+		System.out.println("Sorted Array:");
+		printArray(unsortedArray);
+	}
+	
+	public static void printArray(int[] arrayToPrint){
+	    for(int i = 0; i < arrayToPrint.length; i++){
+	        System.out.print(arrayToPrint[i]+" ");
+	    }
+	    System.out.println();
+	}
 }
